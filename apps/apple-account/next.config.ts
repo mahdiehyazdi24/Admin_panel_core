b42cd6143718c5e9f131b8a0d2ca7990a1c284f0
buggyGrandma <alireza.kh3221@gmail.com>
2025-11-26 14:43:21 +0330

    fix(services): handle proper auth header set on developement

--- apps/apple-account/next.config.ts
+++ apps/apple-account/next.config.ts
@@ -4,6 +4,20 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async rewrites() {
+    // In development, we proxy API and panel-related routes to their respective domains using Next.js rewrites.
+    // This setup mimics our production configuration, where the same routing is handled by Nginx as a reverse proxy.
+    if (process.env.NODE_ENV === "development") {
+      return [
+        {
+          source: "/api/v1/:path*",
+          destination: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/:path*`,
+        },
+      ];
+    }
+
+    return [];
+  },
 };
 
 export default nextConfig;
